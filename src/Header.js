ec1df6cf173b6be7b34d5477c931650844dc6ddf
Govind B Chandran <govindchandran235@gmail.com>
2021-05-20 14:32:11 +0530

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+import PersonIcon from '@material-ui/icons/Person';
+
+function Header() {
+    return (
+        <div className="header">
+        
+            <PersonIcon/>
+            
+        </div>
+    )
+}
+
+export default Header
