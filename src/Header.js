eddc60bd01742ab42257883c2ca293ed189e19b9
Govind B Chandran <govindchandran235@gmail.com>
2021-05-20 14:35:20 +0530

    Add files via upload

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+
 
 function Header() {
     return (
         <div className="header">
-        
-            <PersonIcon fontSize="large" className="header__personIcon" />
+        <IconButton>
+        <PersonIcon fontSize="large" className="header__personIcon" />
+        </IconButton>
             
         </div>
     )
