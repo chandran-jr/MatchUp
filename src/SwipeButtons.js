182aa9b4eaf2d3b1efc38a6d20dca757663eab01
Govind B Chandran <govindchandran235@gmail.com>
2021-05-22 23:37:45 +0530

    Add files via upload

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -9,8 +9,22 @@ import IconButton from '@material-ui/core/IconButton';
 
 function SwipeButtons() {
     return (
-        <div className="swipebuttons">
-            
+        <div className="swipeButtons">
+            <IconButton className="swipeButtons__repeat">
+                <ReplayIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__left">
+                <CloseIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__star">
+                <StarRateIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__right">
+                <FavoriteIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__lightning">
+                <FlashOnIcon fontSize="large" />
+            </IconButton>
         </div>
     )
 }
