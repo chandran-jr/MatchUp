c027819fe824b2352fc10e74ff14230839aa8840
Govind B Chandran <govindchandran235@gmail.com>
2021-05-22 16:28:49 +0530

    Add files via upload

---
+++ src/TinderCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TinderCard.css';
+
+function TinderCard() {
+    return (
+        <div className="tindercard">
+            
+        </div>
+    )
+}
+
+export default TinderCard
