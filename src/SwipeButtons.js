76a90a1b13c96f372b89a82efabddc23914243d4
Govind B Chandran <govindchandran235@gmail.com>
2021-05-22 21:34:09 +0530

    Add files via upload

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './SwipeButtons.css';
+import ReplayIcon from '@material-ui/icons/Replay';
+import CloseIcon from '@material-ui/icons/Close';
+import StarRateIcon from '@material-ui/icons/StarRate';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import IconButton from '@material-ui/core/IconButton';
 
 function SwipeButtons() {
     return (
