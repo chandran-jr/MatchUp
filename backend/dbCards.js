1d39431365373bafa0508e23ab80621652d081a3
Govind B Chandran <govindchandran235@gmail.com>
2021-06-02 00:36:33 +0530

    Add files via upload

---
+++ backend/dbCards.js
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose';
+
+const cardSchema = mongoose.Schema({
+    name: String, 
+    imgUrl: String,
+})
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,14 @@ const app = express();
 
 const port = process.env.PORT || 8001;
 
+const connection_url = 'mongodb+srv://admin:HeAwbm2rtSa6V3e1@cluster0.meofa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
+
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
 
 
 
