018262db8d7d1b6eb76a79ec7015f35511af7018
Govind B Chandran <govindchandran235@gmail.com>
2021-06-03 01:20:04 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import Cards from './dbCards.js'
+import Cards from './dbCards.js';
+import Cors from 'cors';
 
 const app = express();
 
 const port = process.env.PORT || 8001;
 
-const connection_url = 'mongodb+srv://admin:HeAwbm2rtSa6V3e1@cluster0.meofa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const connection_url = 'mongodb+srv://admin:HeAwbm2rtSa6V3e1@cluster0.meofa.mongodb.net/matchupdb?retryWrites=true&w=majority';
 
+
+app.use(express.json());
+app.use(Cors());
 
 mongoose.connect(connection_url, {
     useNewUrlParser: true,
@@ -31,6 +35,18 @@ app.post('/tinder/card', (req, res) => {
         }
         else {
             res.status(201).send(data);
+        }
+    })
+});
+
+app.get('/tinder/card', (req, res) => {
+
+    Cards.find((error, data) => {
+        if(error) {
+            res.status(500).send(error);
+        }
+        else {
+            res.status(200).send(data);
         }
     })
 })
