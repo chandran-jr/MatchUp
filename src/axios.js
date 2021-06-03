3760113ce4c6a6efca82b2123f99a55943eb24f4
Govind B Chandran <govindchandran235@gmail.com>
2021-06-03 22:43:33 +0530

    Add files via upload

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8001'
-})
+    baseURL: 'https://matchupdb.herokuapp.com',
+});
 
 export default instance;
